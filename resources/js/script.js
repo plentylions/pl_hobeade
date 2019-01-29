2016c2f5397025f85ed7fe8f56810c12763e0c12
Misha Yatsenko <1234asdfm@gmail.com>
2019-01-29 11:36:25 +0200

    3.0.1 test

--- resources/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("afterCategoryChanged", function (e) {
-  const currentCategory = e.detail.currentCategory;
-  const categoryTitle = currentCategory.details[0].metaTitle ?
-                            currentCategory.details[0].metaTitle :
-                            currentCategory.details[0].name;
-
-  document.title = categoryTitle;
-}, false);
