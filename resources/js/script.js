206175b8df7cfe623c2fadccdd04f4bf243ec77c
plentylionsgit <info@plentylions.de>
2019-07-10 14:17:41 +0300

    dev

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,4 +3,17 @@ $(function () {
         $( ".searchCol .search-box-inner" ).slideToggle( "slow", function() { });
         return false;
     });
+
+    if ($('#page-header').attr('data-sticky') == 'true') {
+        // Check the initial Poistion of the Sticky Header
+        var stickyHeaderTop = $('#page-header').offset().top;
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > stickyHeaderTop && $('#page-header').is(':visible')) {
+                $('#page-header').addClass('sticky');
+            } else {
+                $('#page-header').removeClass('sticky');
+            }
+        });
+    }
 });
